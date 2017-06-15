7a9f4851378e51a6e0b9e1d1297d8fbebd4c4e7f
Clément Escolano <clement.escolano@icloud.com>
2017-06-15 21:21:57 +0200

    proposition de films 2

--- recommendation.py
+++ recommendation.py
@@ -75,7 +75,7 @@ class Recommendation:
 
     # Pose une question à l'utilisateur
     def ask_question(self, user):
-        return ("Avez-vous aimé " + choice(movies_list))
+        return ("Avez-vous aimé " + choice(self.movies_list))
 
     # Calcule la similarité entre 2 utilisateurs
     @staticmethod
